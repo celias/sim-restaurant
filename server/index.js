d17ae809609e723f23e42ea54e09490aff969a79
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-03-23 10:28:22 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const {json} = require('body-parser');
+const cors = require('cors');
+
+const port = 3001;
+const app = express();
+
+app.use(json());
+app.use(cors());
+
+app.listen(port, () => {
+    console.log(`Listening to sweetness on port: ${port}`);
+});
+
